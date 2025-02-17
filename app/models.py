88202f92be7b7ef4c1dc3d66867cb4288961fe7b
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-02-16 21:51:48 -0300

    feat: add Cadastro model with fields for name, age, and registration date

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cadastro(models.Model):
+    nome = models.CharField(max_length=250)
+    idade = models.IntegerField()
+    data_cadastro = models.DateField(auto_now_add=True)
+    
