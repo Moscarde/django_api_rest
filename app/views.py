9f8824678651c3741be2e318148f48fc62a82ab8
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-02-16 22:19:57 -0300

    feat: enhance cadastros view to support POST requests for creating new records

--- app/views.py
+++ app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
 
 from .models import Cadastro
 from .serializer import CadastroSerializer
 
+
 # Create your views here.
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def cadastros(request):
-    pessoa = Cadastro.objects.all()
-    serializer = CadastroSerializer(pessoa, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        pessoa = Cadastro.objects.all()
+        serializer = CadastroSerializer(pessoa, many=True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        serializer = CadastroSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
