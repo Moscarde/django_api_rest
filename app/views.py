e7766af276330845644b6c633291a79ec58cc5f7
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-02-16 22:08:25 -0300

    feat: add API view for retrieving Cadastro records

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Cadastro
+from .serializer import CadastroSerializer
 
 # Create your views here.
+@api_view(http_method_names=["GET"])
+def cadastros(request):
+    pessoa = Cadastro.objects.all()
+    serializer = CadastroSerializer(pessoa, many=True)
+    return Response(serializer.data)
