8fd1ea868a0f63d161b1fef348ca15bfdc5ef803
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-02-16 22:44:48 -0300

    feat: add URL routing for consulta_att_delete view in app/urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
+
 from . import views
 
-urlpatterns = [
-    path("", views.cadastros)
-]
+urlpatterns = [path("", views.cadastros), path("<int:id>", views.consulta_att_delete)]
